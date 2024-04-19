e944e80bcad72803736f85fceb856aa89f8fa9f0
ASTM1999 <anusornsriprom1999@gmail.com>
2024-04-19 18:05:57 +0700

    connect mongoDB compass success!!!

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://anusornsriprom44:AMgxxhOKIHmlXUua@cluster0.bmiggf1.mongodb.net/sample_mflix'),
+
+    // TypeOrmModule.forRoot({
+    //   type: 'mongodb',
+    //   url: 'mongodb+srv://anusornsriprom44:AMgxxhOKIHmlXUua@cluster0.bmiggf1.mongodb.net/',
+    //   // database:"totalCalendar",
+    //   database: "supervision",
+    //   synchronize: true,
+    //   logging: true,
+    //   entities: [
+    //     Users,
+
+    //   ],
+    // }),
+    // MongooseModule.forRoot('mongodb+srv://anusornsriprom44:AMgxxhOKIHmlXUua@cluster0.bmiggf1.mongodb.net/'),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
