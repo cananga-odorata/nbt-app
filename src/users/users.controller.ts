3351b3227982b98078324e62ed86462d4f774f1a
ASTM1999 <anusornsriprom1999@gmail.com>
2024-04-19 18:07:45 +0700

    create function, schema Get() of Users module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { Users } from './users.interface';
+
 
 @Controller('users')
 export class UsersController {
 
     constructor(private readonly usersService: UsersService) { }
+
     @Get()
     getHelloUser(): string {
         return this.usersService.getHelloUser();
+    }
+
+    @Get('/all')
+    async getAllUser(): Promise<any[]> {
+        return await this.usersService.getAllUser()
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+
 
 @Injectable()
 export class UsersService {
 
-    getHelloUser():string{
-        return 'Hello user';
+    constructor(
+        @InjectModel('users')
+        private readonly usersModel: Model<any>,
+    ) { }
+
+    async getAllUser() {
+        return await this.usersModel.find({}).sort({ score: 'desc' }).exec();
+    }
+
+    getHelloUser(): string {
+        return 'Hello user test';
     }
 }
